420908dfecffc0842e01c7ba8231767c94cd8b1c
ai 女王的男人❤ <571831158@qq.com>
2016-08-04 00:16:30 +0800

    init

---
+++ gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by 程恒 on 16/8/4.
+//  Copyright © 2016年 程恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest
+//
+//  Created by 程恒 on 16/8/4.
+//  Copyright © 2016年 程恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
